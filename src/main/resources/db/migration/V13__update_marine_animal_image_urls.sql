66de01a7c810b038f73b383db3dec5b2cda55813
Wang <wang@mini-pro.lan>
2025-02-03 19:09:14 +0800

    完成了静态页面图片显示

---
+++ src/main/resources/db/migration/V13__update_marine_animal_image_urls.sql
@@ -0,0 +1,5 @@
+UPDATE marine_animal SET image_url = '/images/animals/blue-whale.jpg' WHERE name = '蓝鲸';
+UPDATE marine_animal SET image_url = '/images/animals/dolphin.jpg' WHERE name = '海豚';
+UPDATE marine_animal SET image_url = '/images/animals/green-turtle.jpg' WHERE name = '绿海龟';
+UPDATE marine_animal SET image_url = '/images/animals/coral.jpg' WHERE name = '珊瑚';
+UPDATE marine_animal SET image_url = '/images/animals/humpback-whale.jpg' WHERE name = '座头鲸'; 
